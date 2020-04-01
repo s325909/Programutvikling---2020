d813541619529b9706aea57068a67d7318149c5b
Athisaiyan <athisaiyan@hotmail.com>
2020-04-02 01:19:34 +0200

    forenkling av kode for knapp fikset
    har nå fikset å forenkle kode , og brukt toggle group for bruk av Radiobuttons , den som gjør at du kun velgerr en. En toggle group er en gruppe  av radiobuttons. eks : biltypeknappene... , har definert de og  klart å hente riktig data fra både biltype og komponent. Må se på hvordan man henter navn,pris... fra den nye klassen til Jaso

--- src/org/ccomp/admin/LeggeTilController.java
+++ src/org/ccomp/admin/LeggeTilController.java
@@ -6,39 +6,37 @@ import java.util.ResourceBundle;
 //import com.sun.tools.javac.comp.Todo;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.ccomp.model.Car;
 import org.ccomp.model.component.engine.ElectricMotor;
 
 
 public class LeggeTilController implements Initializable {
-    String radiotext;
+    String komponentType;
     String bilTypetext;
     String motorNavn;
     String hesteKreftertxt;
     String pristxt;
 
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
-//bilType
+
+    //bilType
     @FXML
-    RadioButton bensin,elBil,hybrid;
+    RadioButton bensin, elBil, hybrid;
+
 
     //komponent
     @FXML
-    RadioButton motor,seteTrekk,felg;
+    RadioButton motor, seteTrekk, felg;
 
-   //Skrive inn
+    //Skrive inn
 
     @FXML
-    TextField navn,hestekrefter,pris;
+    TextField navn, hestekrefter, pris;
 
     @FXML
     TextArea skrivUt;
@@ -47,138 +45,65 @@ public class LeggeTilController implements Initializable {
     @FXML
     Button printUt;
 
+    @FXML
+    ToggleGroup carTypeGroup,componentTypeGroup;
 
 
-   @FXML
+    @FXML
     void utText() {
 
-       ElectricMotor electricMotor = new ElectricMotor();
-       electricMotor.engineName(navn.getText());
-       electricMotor.enginePower(Integer.parseInt(hestekrefter.getText()));
-       electricMotor.enginePrice(Integer.parseInt(pris.getText()));
+        ElectricMotor electricMotor = new ElectricMotor();
+        electricMotor.engineName(navn.getText());
+        electricMotor.enginePower(Integer.parseInt(hestekrefter.getText()));
+        electricMotor.enginePrice(Integer.parseInt(pris.getText()));
 
 
-       Car car = new Car(electricMotor);
-       car.builCarEngine();
+        Car car = new Car(electricMotor);
+        car.builCarEngine();
 
-       String radiotext= "";
-       String bilTypetext = "";
-       String motorNavn = electricMotor.getEngineName();//navn.getText();
-       String hesteKreftertxt = String.valueOf(electricMotor.getHorsePower());//hestekrefter.getText();
-       String pristxt = String.valueOf(electricMotor.getEnginePrice());//pris.getText();
+        String komponentType = "";
+        String bilTypetext = "";
+        String motorNavn = electricMotor.getEngineName();//navn.getText();
+        String hesteKreftertxt = String.valueOf(electricMotor.getHorsePower());//hestekrefter.getText();
+        String pristxt = String.valueOf(electricMotor.getEnginePrice());//pris.getText();
 
-
-       if (elBil.isSelected()) {
-           bilTypetext = elBil.getText();
-           Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-           skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
-                   "Komponent: " + bil1.getKomponenter() + "\n" +
-                   "Navn :  " +  bil1.getNavn() + "\n" +
-                   "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                   "Pris: " + bil1.getPris()
-           );
-
-
-       }
-
-       if(hybrid.isSelected()){
-           bilTypetext = hybrid.getText();
-           Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-           skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
-                   "Komponent: " + bil1.getKomponenter() + "\n" +
-                   "Navn :  " +  bil1.getNavn() + "\n" +
-                   "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                   "Pris: " + bil1.getPris()
-           );
-
-       }
-
-
-       //motor
-       if (motor.isSelected()){
-           radiotext = motor.getText();
-           Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-           skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
-                   "Komponent: " + bil1.getKomponenter() + "\n" +
-                   "Navn :  " +  bil1.getNavn() + "\n" +
-                   "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                   "Pris: " + bil1.getPris()
-           );
-       }
-
-       if (seteTrekk.isSelected()){
-
-           radiotext = seteTrekk.getText();
-           Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-           skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
-                   "Komponent: " + bil1.getKomponenter() + "\n" +
-                   "Navn :  " +  bil1.getNavn() + "\n" +
-                   "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                   "Pris: " + bil1.getPris()
-           );
-       }
-
-       if(felg.isSelected()){
-
-           radiotext = felg.getText();
-           Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-           skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
-                   "Komponent: " + bil1.getKomponenter() + "\n" +
-                   "Navn :  " +  bil1.getNavn() + "\n" +
-                   "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                   "Pris: " + bil1.getPris()
-           );
-
-       }
-
-
-
-
-
-
-
-       //  String bensinChoosen = bensin.getText();
-
-
-
-
-     // Bil bil1 = new Bil(bensinChoosen,motor.getText(),navn.getText(),hestekrefter.getText(),pris.getText());
-
-      //skrivUt.setText("Biltype:" + bil1.getBilType());
-
+        if (carTypeGroup.getSelectedToggle().isSelected() && componentTypeGroup.getSelectedToggle().isSelected() ) {
+            valgKnapp();
+        }
 
 
     }
 
-
-   /* public  void knappValg(RadioButton knapptype){
-        bilTypetext = knapptype.getText();
-        Bil bil1 = new Bil(bilTypetext,radiotext,motorNavn,hesteKreftertxt,pristxt);
-        skrivUt.setText("Bil: " + bil1.getBilType()+ "\n" +
+    public void valgKnapp(){
+        bensin.setUserData("Bensin");
+        elBil.setUserData("Elbil");
+        hybrid.setUserData("Hybrid");
+        motor.setUserData("Motor");
+        seteTrekk.setUserData("Setetrekk");
+        felg.setUserData("Felg");
+        bilTypetext = carTypeGroup.getSelectedToggle().getUserData().toString();
+        komponentType = componentTypeGroup.getSelectedToggle().getUserData().toString() ;
+        Bil bil1 = new Bil(bilTypetext, komponentType, motorNavn, hesteKreftertxt, pristxt);
+        skrivUt.setText("Bil: " + bil1.getBilType() + "\n" +
                 "Komponent: " + bil1.getKomponenter() + "\n" +
-                "Navn :  " +  bil1.getNavn() + "\n" +
-                "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
+                "Navn :  " + bil1.getNavn()+ "\n" +
+                "Hestekrefter :" +  bil1.getHestekrefter() + "\n" +
                 "Pris: " + bil1.getPris()
         );
 
     }
-*/
 
-   /* //Kolonnene
-    ArrayList<String> biltype = new ArrayList<String>();
+    /*public void knappValg(RadioButton knapptype) {
+        bilTypetext = knapptype.getText();
+        Bil bil1 = new Bil(bilTypetext, radiotext, motorNavn, hesteKreftertxt, pristxt);
+        skrivUt.setText("Bil: " + bil1.getBilType() + "\n" +
+                "Komponent: " + bil1.getKomponenter() + "\n" +
+                "Navn :  " + bil1.getNavn() + "\n" +
+                "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
+                "Pris: " + bil1.getPris()
+        );
 
-
-    ArrayList<String> motor = new ArrayList<>();
-    ArrayList<String> setetrekk = new ArrayList<>();
-    ArrayList<String> felg = new ArrayList<>();
-    ArrayList<String> ratt = new ArrayList<>();
-*/
-
-
-
-
-
-
+    }*/
 }
 
 
