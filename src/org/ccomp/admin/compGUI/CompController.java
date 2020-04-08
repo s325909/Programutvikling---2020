57d09a23d455c6efb5699172516dbb264e19eed8
Athisaiyan <athisaiyan@hotmail.com>
2020-04-08 14:49:41 +0200

    rydding av CompController
    De som er igjen , er de dere kan bruke, fjernet det unødvendinge som ikke vil blir brukt lenger. De andre metodene kan vidreutvikles for arraylist evt.

--- src/org/ccomp/admin/compGUI/CompController.java
+++ src/org/ccomp/admin/compGUI/CompController.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.ccomp.model.component.Seat;
+import org.ccomp.model.component.Spoiler;
 import org.ccomp.model.component.engine.ElectricMotor;
 import org.ccomp.model.component.engine.Engine;
 import org.ccomp.model.component.engine.GasolineEngine;
@@ -50,55 +52,28 @@ public class CompController implements Initializable {
     TextField navn, /*horsepower*/variable, price;
     //her må det komme inn en felles verdig for tekstfelt 2
 
-    @FXML
-    TextArea skrivUt;
-
     //leggtilKnapp
     @FXML
-    Button printUt, backToAdd;
+    Button backToAdd;
 
     @FXML
     ToggleGroup carTypeGroup, componentTypeGroup;
 
 
     @FXML
-    void utText() {
+    //Metod for hvis begge knappene er valgt. Altså biltype & komponent.
+    /*void utText() {
 
-        /*
-        ElectricMotor electricMotor = new ElectricMotor();
-        electricMotor.engineName(navn.getText());
-        electricMotor.enginePower(Integer.parseInt(hestekrefter.getText()));
-        electricMotor.enginePrice(Integer.parseInt(pris.getText()));
-
-         */
-
-
-        // Car car = new Car(electricMotor);
-        // car.builCarEngine();
-
-      /*  String komponentType = "";
-        String bilTypetext = "";
-        String engineName = electricMotor.getEngineName();//navn.getText();
-        String hesteKreftertxt = String.valueOf(electricMotor.getHorsePower());//hestekrefter.getText();
-        String pristxt = String.valueOf(electricMotor.getEnginePrice());//pris.getText();*/
-
-      /* if (carTypeGroup.getSelectedToggle().isSelected() && componentTypeGroup.getSelectedToggle().isSelected() ) {
+       if (carTypeGroup.getSelectedToggle().isSelected() && componentTypeGroup.getSelectedToggle().isSelected() ) {
             selectedCarType();
 
-
-        }*/
+        }
 
 
         selectedCarType();
 
 
-        //skrivUt.setText("Biltype:" + namegivingEngine + "\n");
-
-
-        //if elbil
-        //kaller selectedENgine
-        // new electro motor , etc
-    }
+    }*/
 
 
     private void selectedCarType() {
@@ -122,8 +97,20 @@ public class CompController implements Initializable {
         }
     }
 
+    private Engine selectedEngine(Engine engine) {
 
-    /*private void selectedAthoerComp(){
+        namegivingEngine = navn.getText();
+        horsePowertxt = String.valueOf(variable.getText());
+        pricetxt = price.getText();
+        engine.engineName(namegivingEngine);
+        engine.enginePower(Integer.parseInt(horsePowertxt));
+        engine.enginePrice(Integer.parseInt(pricetxt));
+        return engine;
+    }
+
+
+    //kode for hvis sete er valgt TODO må gjøre om på variabler ,opp til deg im du vil bruke den.
+   /* private void selectedAthoerComp(){
         if (seat.isSelected()){
             Seat seat1 = new Seat();
             selectedSeat(seat1);
@@ -137,7 +124,7 @@ public class CompController implements Initializable {
         }*/
 
 
-    //TODO må bare forsette videre med flere komp
+    //TODO må bare forsette videre med flere komponenter, er bare å legge det inn med arraylist
 
 
 
@@ -164,51 +151,12 @@ public class CompController implements Initializable {
 
     }
 */
-    private Engine selectedEngine(Engine engine) {
-
-        namegivingEngine = navn.getText();
-        horsePowertxt = String.valueOf(variable.getText());
-        pricetxt = price.getText();
-        engine.engineName(namegivingEngine);
-        engine.enginePower(Integer.parseInt(horsePowertxt));
-        engine.enginePrice(Integer.parseInt(pricetxt));
-        return engine;
-    }
-
-
-   /* public void valgKnapp(){
-        bensin.setUserData("Bensin");
-        elBil.setUserData("Elbil");
-        hybrid.setUserData("Hybrid");
-        motor.setUserData("Motor");
-        seteTrekk.setUserData("Setetrekk");
-        felg.setUserData("Felg");
-        bilTypetext = carTypeGroup.getSelectedToggle().getUserData().toString();
-        komponentType = componentTypeGroup.getSelectedToggle().getUserData().toString() ;
-        Bil bil1 = new Bil(bilTypetext, komponentType, motorNavn, hesteKreftertxt, pristxt);
-        skrivUt.setText("Bil: " + bil1.getBilType() + "\n" +
-                "Komponent: " + bil1.getKomponenter() + "\n" +
-                "Navn :  " + bil1.getNavn()+ "\n" +
-                "Hestekrefter :" +  bil1.getHestekrefter() + "\n" +
-                "Pris: " + bil1.getPris()
-        );*/
 
 
 
 
 
-    /*public void knappValg(RadioButton knapptype) {
-        bilTypetext = knapptype.getText();
-        Bil bil1 = new Bil(bilTypetext, radiotext, motorNavn, hesteKreftertxt, pristxt);
-        skrivUt.setText("Bil: " + bil1.getBilType() + "\n" +
-                "Komponent: " + bil1.getKomponenter() + "\n" +
-                "Navn :  " + bil1.getNavn() + "\n" +
-                "Hestekrefter :" + bil1.getHestekrefter() + "\n" +
-                "Pris: " + bil1.getPris()
-        );
-
-    }*/
-
+    //Tilbake knapp
     @FXML
     public void backToAddComp() {
         try {
