e0012eb48e604405b7a2f5e6c00a805220c78153
Athisaiyan <athisaiyan@hotmail.com>
2020-04-17 01:45:29 +0200

    presentasjon av komponenter
    Har nå fikset at alle komponenter kommer opp på tableview, og får også opp etter registrert, må jobbe  videre med endring , oppdartering og sletting. Tror den oppdaterseg. Men må kunne oppdatere når man endrer og sletter

--- src/org/ccomp/admin/AdminController.java
+++ src/org/ccomp/admin/AdminController.java
@@ -8,15 +8,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
 import javafx.stage.Stage;
 import org.ccomp.fileHandling.ComponentOBJHandler;
 import org.ccomp.model.Car;
 import org.ccomp.model.component.CarComponent;
 import org.ccomp.model.component.Seat;
+import org.ccomp.model.component.Spoiler;
+import org.ccomp.model.component.SteeringWheel;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -32,10 +34,20 @@ public class AdminController implements Initializable {
     Button addComp;
 
     @FXML
-    TableView<Seat> tableview;
+    TableView<Seat> seatView;
+    @FXML
+    TableView<Spoiler> spoilerView;
+    @FXML
+    TableView<SteeringWheel> sWheelView;
 
     @FXML
-    TableColumn<Seat, SimpleStringProperty> materiellColumn,nameColumn;
+    TableColumn<Seat, SimpleStringProperty> nameSeatColum,materiellColum,colorSeatColum,seatPriceColum,quantitySeatColum;
+    @FXML
+    TableColumn<Spoiler, SimpleStringProperty> nameSpoilerColum,colorSpoilerColum,sideSpoilerColum,priceSpoilerColum,quantitySpoilerColum;
+
+    @FXML
+    TableColumn<SteeringWheel,SimpleStringProperty> nameSWheelColum,materiellSWeel,colorSWheelColum,priceSWeelColum,quantitySWeelColum;
+
 
 
 
@@ -46,9 +58,18 @@ public class AdminController implements Initializable {
 
 
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        seatView.setItems(seatTable());
+        spoilerView.setItems(spoilerTable());
+        sWheelView.setItems(sWheelTable());
+        seatView.setEditable(true);
+        spoilerView.setEditable(true);
+        sWheelView.setEditable(true);
+
+
+
 
     }
 
@@ -86,7 +107,7 @@ public class AdminController implements Initializable {
     }
 
 
-    public ObservableList<Seat> presentTable() {
+    public ObservableList<Seat> seatTable() {
 
        Seat seat;
        ObservableList<Seat> seats=  FXCollections.observableArrayList();
@@ -97,14 +118,59 @@ public class AdminController implements Initializable {
 
         for (CarComponent carComponent : carComponents) {
             seat = (Seat) carComponent;
-            materiellColumn.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("material"));
-            nameColumn.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("compName"));
-
-
-
-
+            nameSeatColum.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("compName"));
+            materiellColum.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("material"));
+            colorSeatColum.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("color"));
+            seatPriceColum.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("compPrice"));
+            quantitySeatColum.setCellValueFactory(new PropertyValueFactory<Seat,SimpleStringProperty>("compQuantity"));
+            seats.add(seat);
         }
+
         return seats;
+    }
+
+    public ObservableList<Spoiler> spoilerTable(){
+
+        Spoiler spoiler;
+        ObservableList<Spoiler> spoilers = FXCollections.observableArrayList();
+        jobjHandler = new ComponentOBJHandler();
+        retrievedCompMap = jobjHandler.readComponent(retrievedCompMap);
+        carComponents = retrievedCompMap.get("Spoiler");
+
+        for (CarComponent carComponent : carComponents) {
+            spoiler = (Spoiler) carComponent;
+            nameSpoilerColum.setCellValueFactory(new PropertyValueFactory<Spoiler,SimpleStringProperty>("compName"));
+           // colorSpoilerColum.setCellValueFactory(new PropertyValueFactory<Spoiler,SimpleStringProperty>("compName"));
+            sideSpoilerColum.setCellValueFactory(new PropertyValueFactory<Spoiler,SimpleStringProperty>("spoilerSide"));
+            priceSpoilerColum.setCellValueFactory(new PropertyValueFactory<Spoiler,SimpleStringProperty>("compPrice"));
+            quantitySpoilerColum.setCellValueFactory(new PropertyValueFactory<Spoiler,SimpleStringProperty>("compQuantity"));
+            spoilers.add(spoiler);
+        }
+
+        return spoilers;
+    }
+
+
+    public ObservableList<SteeringWheel> sWheelTable(){
+
+        SteeringWheel steeringWheel;
+        ObservableList<SteeringWheel> steeringWheels = FXCollections.observableArrayList();
+        jobjHandler = new ComponentOBJHandler();
+        retrievedCompMap = jobjHandler.readComponent(retrievedCompMap);
+        carComponents = retrievedCompMap.get("SteeringWheel");
+
+
+        for (CarComponent carComponent : carComponents) {
+            steeringWheel = (SteeringWheel) carComponent;
+            nameSWheelColum.setCellValueFactory(new PropertyValueFactory<SteeringWheel,SimpleStringProperty>("compName"));
+            colorSWheelColum.setCellValueFactory(new PropertyValueFactory<SteeringWheel,SimpleStringProperty>("compName"));
+            materiellSWeel.setCellValueFactory(new PropertyValueFactory<SteeringWheel,SimpleStringProperty>("spoilerSide"));
+            priceSWeelColum.setCellValueFactory(new PropertyValueFactory<SteeringWheel,SimpleStringProperty>("compPrice"));
+            quantitySWeelColum.setCellValueFactory(new PropertyValueFactory<SteeringWheel,SimpleStringProperty>("compQuantity"));
+            steeringWheels.add(steeringWheel);
+        }
+
+        return steeringWheels;
     }
 
 
@@ -112,7 +178,10 @@ public class AdminController implements Initializable {
 
 
 
+   /* public void changeseatTable(TableColumn.CellEditEvent<Seat, SimpleStringProperty> seatSimpleStringPropertyCellEditEvent) {
+        Seat seatselected = seatView.getSelectionModel().getSelectedItem();
+        seatselected.setCompName(seatSimpleStringPropertyCellEditEvent.getNewValue().toString());
 
 
-
+    }*/
 }
