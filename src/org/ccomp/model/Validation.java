086d1147c02f4d12fa2174720882321e23440449
LindusheK <s333751@oslomet.no>
2020-04-22 18:47:13 +0200

    Lagt til personklasse og lagt til metoder i validation

--- src/org/ccomp/model/Validation.java
+++ src/org/ccomp/model/Validation.java
@@ -9,9 +9,9 @@ public class Validation {
                 +validateMaterial(material) + validateSide(side) + validateDimension(dimension) + validateObjectCarType(carType) + validateObjectOnNull(quantity);
     }
 
-    public static String valUser(String name, String email, String number) {
+    public static String valUser(String name, String email, String number, String PoNr, String city) {
 
-        return validateName(name) + validateEmail(email) + validateNumber(number);
+        return validateName(name) + validateEmail(email) + validateNumber(number) + validatePoNr(PoNr) + validateCity(city);
     }
 
     public static String validateName(String input) {
@@ -20,14 +20,14 @@ public class Validation {
         String regex = "[a-zæøåA-ZÆØÅ]*";
 
         if (!input.matches(regex)) {
-            lable = ("Navnet er i feil format! \n");
+            lable = ("Navnet er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn navn på komponentet! \n");
         }
         return lable;
     }
 
-    public static String validateQuantity (String input) {
+  /*  public static String validateQuantity (String input) {
         String lable = "";
 
         String regex = "[0-9]\\d{0,5}";
@@ -38,7 +38,7 @@ public class Validation {
             lable = ("Vennligst fyll inn riktig antall komponenter! \n");
         }
         return lable;
-    }
+    }*/
 
     public static String validatePrice (String input) {
         String lable = "";
@@ -46,7 +46,7 @@ public class Validation {
         String regex = "[0-9]\\d{3,5}";
 
         if (!input.matches(regex)) {
-            lable = ("Feil format i pris!\n");
+            lable = ("Pris er skrevet inn i feil format!\n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn pris for komponentet! \n");
         }
@@ -59,7 +59,7 @@ public class Validation {
         String regex = "[0-9]\\d{0,3}";
 
         if (!input.matches(regex)) {
-            lable = ("Antall hestekrefter er i feil format!\n");
+            lable = ("Antall hestekrefter er skrevet inn i feil format!\n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn antallet hestekrefter for komponent!\n");
         }
@@ -72,7 +72,7 @@ public class Validation {
         String regex = "[a-zæøåA-ZÆØÅ]{3,10}";
 
         if (!input.matches(regex)) {
-            lable = ("Farge er i feil format! \n");
+            lable = ("Farge er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn en farge på komponentet! \n");
         }
@@ -85,7 +85,7 @@ public class Validation {
         String regex = "[a-zæøåA-ZÆØÅ]";
 
         if (!input.matches(regex)) {
-            lable = ("Materiale er i feil format! \n");
+            lable = ("Materiale er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn materiale på komponentet! \n");
         }
@@ -98,7 +98,7 @@ public class Validation {
         String regex = "(Høyre | høyre | Venstre | venstre)";
 
         if (!input.matches(regex)) {
-            lable = ("Side på spoiler er i feil format! \n");
+            lable = ("Side på spoiler er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn monteringsside for komponentet! \n");
         }
@@ -111,7 +111,7 @@ public class Validation {
         String regex = "[0-9]{2}| x | [0-9]{2}";
 
         if (!input.matches(regex)) {
-            lable = ("Dimensjonen på felg er i feil format! \n");
+            lable = ("Dimensjonen på felg er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn dimensjonen for komponentet! \n");
         }
@@ -134,7 +134,7 @@ public class Validation {
         String regex = "[0-9]{8}";
 
         if (!input.matches(regex)) {
-            lable = ("Du har ikke skrevet inn riktig format for mobilnummer! \n");
+            lable = ("Mobilnummer er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn ditt mobilnummer! \n");
         }
@@ -147,9 +147,35 @@ public class Validation {
         String regex = "[a-zæøåA-ZÆØÅ.-_]*[@][a-zæøåA-ZÆØÅ]*[.]{1}[a-zA-Z]{2,3}";
 
         if (!input.matches(regex)) {
-            lable = ("Du har ikke skrevet inn riktig format for e-post! \n");
+            lable = ("E-postadresse er skrevet inn i feil format! \n");
         } else if (input.isEmpty()) {
             lable = ("Vennligst fyll inn din e-postadresse! \n");
+        }
+        return lable;
+    }
+
+    public static String validatePoNr(String input) {
+        String lable = "";
+
+        String regex = "[0-9]{4}";
+
+        if (!input.matches(regex)) {
+            lable = ("Postnummer er skrevet inn i feil format! \n");
+        } else if (input.isEmpty()) {
+            lable = ("Vennligst fyll inn ditt postnummer! \n");
+        }
+        return lable;
+    }
+
+    public static String validateCity(String input) {
+        String lable = "";
+
+        String regex = "[a-zæøåA-ZÆØÅ]*";
+
+        if (!input.matches(regex)) {
+            lable = ("Poststed er skrevet inn i feil format! \n");
+        } else if (input.isEmpty()) {
+            lable = ("Vennligst fyll inn ditt poststed! \n");
         }
         return lable;
     }
---
+++ src/org/ccomp/user/Customer.java
@@ -0,0 +1,40 @@
+package org.ccomp.user;
+
+public class Customer {
+
+    private String fullName;
+    private String emailadress;
+    private String number;
+    private String zipcode;
+    private String city;
+
+
+    public Customer (String fullName, String emailadress, String number, String zipcode, String city) {
+        this.fullName = fullName;
+        this.emailadress = emailadress;
+        this.number = number;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmailadress() {
+        return emailadress;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
