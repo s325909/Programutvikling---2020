e7de629c7c10cf6e1a59b4a03748c00980e1bb14
Jaso Cyril <s325909@stud.hioa.no>
2020-05-11 13:15:22 +0200

    Lagt til getetre for CarComponent i CompOrder

--- src/org/ccomp/model/CompOrder.java
+++ src/org/ccomp/model/CompOrder.java
@@ -1,14 +1,21 @@
 package org.ccomp.model;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
 import org.ccomp.model.component.CarComponent;
 
 import java.util.List;
 
 public class CompOrder {
 
+    private transient StringProperty compType;
+    private transient StringProperty compName;
+    private transient DoubleProperty compPrice;
+    private transient IntegerProperty compQuantity;
+
     private int orderId;
     private CarComponent carComponent;
-    private List<CarComponent> carComponents;
 
     public CompOrder(int orderId, CarComponent carComponent) {
         this.orderId = orderId;
@@ -17,6 +24,26 @@ public class CompOrder {
 
     public String toCSVFormat() {
         return orderId + "," + carComponent.toCSVFormat();
+    }
+
+    public String getOrderNr() {
+        return String.valueOf(orderId);
+    }
+
+    public String getCompType() {
+        return carComponent.getCompType();
+    }
+
+    public String getCompName() {
+        return carComponent.getCompName();
+    }
+
+    public double getCompPrice() {
+        return carComponent.getCompPrice();
+    }
+
+    public int getCompQuantity() {
+        return carComponent.getCompQuantity();
     }
 
 }
